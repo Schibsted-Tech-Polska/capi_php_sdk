f429ff03b74fa9b075e3d2672b18e27682a319fd
Rafał Kański <rafalkanski@iMac-Rafal.local>
2016-07-26 10:25:35 +0200

    SEC-513 refactoring template method

--- src/Snt/Capi/Repository/Section/SectionRepository.php
+++ src/Snt/Capi/Repository/Section/SectionRepository.php
@@ -5,20 +5,29 @@ namespace Snt\Capi\Repository\Section;
 use Snt\Capi\Http\Exception\ApiHttpClientException;
 use Snt\Capi\Repository\AbstractRepository;
 use Snt\Capi\Repository\FindParametersInterface;
+use Snt\Capi\Repository\RepositoryDictionary;
 
 class SectionRepository extends AbstractRepository implements SectionRepositoryInterface
 {
+    use RepositoryDictionary;
+
     /**
      * {@inheritdoc}
      */
     public function findAll(FindParametersInterface $findParameters)
     {
-        try {
-            $sectionsRawData = json_decode($this->makeHttpGetRequest($findParameters), true);
-        } catch (ApiHttpClientException $exception) {
-            $this->throwCouldNotFetchException($exception);
-        }
+        $sectionsRawData = $this->fetch($findParameters);
 
         return isset($sectionsRawData['sections']) ? $sectionsRawData['sections'] : [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function handleExceptionForFindParameters(
+        ApiHttpClientException $exception,
+        FindParametersInterface $findParameters
+    ) {
+        $this->throwCouldNotFetchException($exception);
     }
 }
